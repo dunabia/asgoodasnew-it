4609671a0126c413b98924eac7b25a8fd38f3646
dunabia <161461530+dunabia@users.noreply.github.com>
2025-06-04 05:59:28 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,17 +9,17 @@ import UgcGallery from "./components/UgcGallery";
 
 function App() {
   //Instance ID
-  let InstanceId_Widget = "1141715";
+  let InstanceId_Widget = "1141720";
   let InstanceId_QA = "";
   let InstanceId_SEO = "";
   let InstanceId_Carousel = "";
-  let InstanceId_StarRating = "1141716";
+  let InstanceId_StarRating = "1141721";
   let InstanceId_PromotedProd = "";
   let InstanceId_UgcGallery = "";
   let InstanceId_ReviewsTab = "";
 
   //Product ID
-  let ProductId = "STAMM-AN69046";
+  let ProductId = "STAMM-AN66596";
 
   //Clipboard text here.
   let ClipBoardHeading = "Follow these steps to add this";
